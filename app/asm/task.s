d78bb3260e13eeddd4fb06bc9d511bd44b120653
eugenereydel99 <eugenereydel@gmail.com>
2021-03-22 20:33:18 +0700

    Lab2 - version 2

--- app/asm/task.s
+++ app/asm/task.s
@@ -1,21 +1,21 @@
 .data
 print_format:
 	.string "Сумма элементов массива = %d\n."
-	
+
 array:
-	.long 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+	.byte 124, 97
 counter:
-	.long 10
+	.byte 2
 
 .text
 .global main
 
-main: 
-        movl counter, %ecx # Количество итераций в цикле for
+main:
+        movsbl counter, %ecx # Количество итераций в цикле for
         movl $array, %ebx # Указатель на текущий элемент массива
         movl $0,%edx   # Сумма элементов массива
-for: 
-        movl (%ebx),%eax # Помещаем текущий элемент массива в регистр eax
+for:
+        movsbl (%ebx),%eax # Помещаем текущий элемент массива в регистр eax
         testl $1,%eax # Проверка элемента массива на четность
         jz if_even # Если элемент четный => переход в метку if_even
         jnz if_odd # Если элемент нечетный => переход в метку if_odd
@@ -23,7 +23,7 @@ for:
 if_even:  # Если четный
         shll $1,%eax  # Логический сдвиг влево на единицу
         addl %eax,%edx # Суммируем в регистр edx текущее значение массива
-        addl $4,%ebx    # Перемещаем указатель на следующий элемент массива
+        addl $1,%ebx  # Перемещаем указатель на следующий элемент массива
         loop for
         jmp print
 
@@ -31,17 +31,13 @@ if_even:  # Если четный
 if_odd:  # Если нечетный
         sarl $1,%eax  # Арифметический сдвиг вправо на единицу
         addl %eax,%edx # Суммируем в регистр edx текущее значение массива
-        addl $4,%ebx
+        addl $1,%ebx
         loop for
         jmp print
 
-print: 
-	pushl %edx
+print:
+        pushl %edx
 	pushl $print_format
 	call printf
 	addl $8, %esp
-	movl $0, %edx
-	movl $0, %ecx
-	movl $0, %ebx
-	movl $0, %eax
         ret
--- app/cpp/task.cpp
+++ app/cpp/task.cpp
@@ -7,17 +7,17 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	unsigned int numbers[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	unsigned char numbers[] = { 124, 197 };
 	int arraySize = sizeof(numbers) / sizeof(*numbers);
 
-	unsigned int sum = 0;
+	unsigned char sum = 0;
 	for (int i = 0; i < arraySize; i++) 
 	{
-		if ((i) % 2 == 0) numbers[i] = numbers[i] << 1;
+		if (numbers[i] % 2 == 0) numbers[i] = numbers[i] << 1;
 		else numbers[i] = numbers[i] >> 1;
 		sum += numbers[i];
 	}
 
-	cout << "Сумма элементов массива = " << sum << endl;
+	cout << "Сумма элементов массива = " << int(sum) << endl;
 	return 0;
 }
